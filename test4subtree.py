22fce2b3aacc53de2e75e01d5b2337068e05cc93
zojusane <2016301200314@whu.edu.cn>
2021-01-29 14:50:01 +0800

    another test

--- test4subtree.py
+++ test4subtree.py
@@ -2,9 +2,10 @@ import numpy as np
 
 class Dog:
 	"""it's a dog"""
-	def __init__(self, name, age,):
+	def __init__(self, name, age, length):
 		self.name = name
 		self.age = age
+		self.length = length
 
 	def sit(self):
 		print(f"a {self.age} dog named {self.name} is sitting down")
